71b96271a27dc6f8441207d39417ed0a5a959f65
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-04-16 17:06:55 +0530

    New Files Added

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "google_compute_instance" "instance_creation" {
+  name         = "sles12test3"
+  machine_type = "e2-small"
+  zone         = "us-east4-c"
+  hostname = "sles12test3.personallab.local"
+  scheduling {
+  preemptible  = true
+  automatic_restart = false
+  }
+  boot_disk {
+    initialize_params {
+      image = "projects/pelagic-magpie-308310/global/images/image-sles12"
+    }
+  }
+
+  network_interface {
+    network = "test-vpc"
+    subnetwork = "test-subnet"
+    #network_ip = "10.0.0.24"
+    access_config {
+    
+    }
+  }
+  metadata = {
+    startup-script = <<SCRIPT
+      #! /bin/bash
+      sudo realm join --verbose PERSONALLAB.LOCAL
+      sudo realm permit -g AccAdminSecOpsServers@PERSONALLAB.LOCAL
+      sudo realm permit -g domain\ admins@PERSONALLAB.LOCAL
+      sudo sh -c "echo 'entry_cache_timeout = 900' >> /etc/sssd/sssd.conf"
+      sudo systemctl restart sssd.service
+      SCRIPT
+  }
+}

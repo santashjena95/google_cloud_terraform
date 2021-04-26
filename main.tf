920c5e473b4fa7131b33947ab99c82f1e9ce6f9a
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-04-26 21:06:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,5 +4,5 @@ module "vm_domain_joined" {
   vm_machine_type = "e2-small"
   vm_zone   = "us-east4-c"
   vm_hostname = "sles12testv.personallab.local"
-  vm_image  = "projects/pelagic-magpie-308310/global/images/sles12image"
+  vm_image  = "projects/pelagic-magpie-308310/global/images/sles12vmimage"
 }

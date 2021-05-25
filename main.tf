a16a5e77e521cece7d82e8a429b2878c4ede389c
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-05-25 20:27:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "vm_domain_joined" {
   source = "./modules"
-  instance_name   = "sles15sp1sap3"
+  instance_name   = "sles15sp1sap4"
   vm_machine_type = "e2-small"
   vm_zone   = "us-east4-c"
-  vm_hostname = "sles15sp1sap3.personallab.local"
+  vm_hostname = "sles15sp1sap4.personallab.local"
   vm_image  = "projects/pelagic-magpie-308310/global/images/sles15sp1sap2"
 }

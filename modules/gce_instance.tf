be41bcde791ed8cb7d1e02af8b889a3961cb73f8
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-05-26 18:41:40 +0530

    Update gce_instance.tf

--- modules/gce_instance.tf
+++ modules/gce_instance.tf
@@ -35,6 +35,7 @@ resource "google_compute_instance" "instance_creation" {
       sudo sed -i 's/use_fully_qualified_names = True/use_fully_qualified_names = False/g' /etc/sssd/sssd.conf
       sudo sh -c "echo 'entry_cache_timeout = 900' >> /etc/sssd/sssd.conf"
       sudo systemctl restart sssd.service
+      sudo reboot
       SCRIPT
     shutdown-script = <<SCRIPT
       #! /bin/bash

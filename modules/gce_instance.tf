3c60fd80d06f6963bd485d9a5c117550f75cfe63
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-05-26 17:18:04 +0530

    Update gce_instance.tf

--- modules/gce_instance.tf
+++ modules/gce_instance.tf
@@ -35,9 +35,6 @@ resource "google_compute_instance" "instance_creation" {
       sudo sed -i 's/use_fully_qualified_names = True/use_fully_qualified_names = False/g' /etc/sssd/sssd.conf
       sudo sh -c "echo 'entry_cache_timeout = 900' >> /etc/sssd/sssd.conf"
       sudo systemctl restart sssd.service
-      SCRIPT
-    shutdown-script = <<SCRIPT
-      #! /bin/bash
       gcloud compute instances remove-metadata ${var.instance_name} --zone=${var.vm_zone} --keys=startup-script
       SCRIPT
   }

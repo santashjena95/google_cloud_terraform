ed45362b9f5455a0cc40e0e325bc31ceac2c8cd8
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2021-05-27 20:39:37 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,6 +7,7 @@ variable "domain_password" {
 variable "instance_name" {
   description = "The name of VM Instance"
   type        = string
+  default     = "sles15sp1sap13"
 }
 variable "vm_machine_type" {
   description = "Machine type of VM Instance"
@@ -21,4 +22,5 @@ variable "vm_zone" {
 variable "vm_image" {
   description = "Image to be used for Boot disk OS"
   type        = string
+  default = "projects/pelagic-magpie-308310/global/images/sles15sp1sapnew"
 }
